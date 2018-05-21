bb32a491355de0666f16bab99069f9b4bb1b2c97
yutao <yutao.xz@foxmail.com>
2018-05-21 16:21:14 +0800

    first commit

---
+++ grape-mail/src/main/java/com/delicacy/grape/mail/GrapeMailApplication.java
@@ -0,0 +1,12 @@
+package com.delicacy.grape.mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrapeMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrapeMailApplication.class, args);
+	}
+}
